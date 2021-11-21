e43f3a02edb883a6468d8be8b8e44e7c93b9e2ba
devshaffan <devshaffan@gmail.com>
2021-11-21 18:26:32 +0500

    Updates

---
+++ precache-manifest.3dc9d92e649c37739491eaed77a56288.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7f5cf946c0f44b33abf",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/css/main.60626b80.chunk.css"
+  },
+  {
+    "revision": "b7f5cf946c0f44b33abf",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/js/main.52b2b7ca.chunk.js"
+  },
+  {
+    "revision": "67d912e551bf1db601b1",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/js/runtime~main.0ab93d23.js"
+  },
+  {
+    "revision": "13bdbc215261d5785997",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/css/2.520c3f7c.chunk.css"
+  },
+  {
+    "revision": "13bdbc215261d5785997",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/js/2.f8ea9c7a.chunk.js"
+  },
+  {
+    "revision": "c9dea4306a63402c284a",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/js/3.245ac885.chunk.js"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "2fd9ac2208c833754484473decfc84a1",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/drop.2fd9ac22.wav"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "57a2639bd6d80374aaf0714a48200f92",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/10.57a2639b.svg"
+  },
+  {
+    "revision": "a7f0e4e3c9b56642615788ea350047d6",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/1.a7f0e4e3.svg"
+  },
+  {
+    "revision": "1ef3b3956becd4d06c1b2be3402c0aa0",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/100.1ef3b395.svg"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "74562e5d9b36cc740a89848ba488bf7d",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/rooster.74562e5d.png"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "8164e48c115fae4bb874ed0c63b40cbc",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/instructions.8164e48c.gif"
+  },
+  {
+    "revision": "612faa13f56ad11ff00e79ec305555fb",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/background.612faa13.gif"
+  },
+  {
+    "revision": "d6687daa9e8311c3106547e339b95f38",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/background2.d6687daa.gif"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "67f6dfc7cde8f57887997ca4621b2cd9",
+    "url": "/urdu_Subtraction_counting_object_3_and_2_digits/index.html"
+  }
+];
